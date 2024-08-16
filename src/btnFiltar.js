a4b0aa6c2c59c435358d47ffbcc0e892af3e5c4d
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2024-08-16 11:41:00 -0500

    versión final

--- src/btnFiltar.js
+++ src/btnFiltar.js
@@ -3,8 +3,29 @@ import cargarPeliculas from "./mostrarDatos";
 
 const btnFiltro= document.getElementById('btn-buscar');
 
+
+
+
 btnFiltro.addEventListener('click',async (e)=>{
-    const datosFiltro= await cargandoDatosFiltros()
+    const generoActivo= document.querySelector('#filtro-generos .btn--active');
+    const contendorBusqueda= document.querySelector('.sidebar__contenedor')
+
+    if (generoActivo) {
+        const datosFiltro= await cargandoDatosFiltros()
     cargarPeliculas(datosFiltro);
     window.scrollTo(0,0)
+    }
+    if (contendorBusqueda) {
+        contendorBusqueda.classList.add('sidebar__contenedor--active')
+        if (generoActivo) {
+        const datosFiltro= await cargandoDatosFiltros()
+    cargarPeliculas(datosFiltro);
+    window.scrollTo(0,0)
+
+    setTimeout(()=>{
+        contendorBusqueda.classList.remove('sidebar__contenedor--active')
+    },500)
+    
+    }
+        }
 });
